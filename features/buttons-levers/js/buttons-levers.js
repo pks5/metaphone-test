494e636a271dfee4e38fe3142525c0a9b1650284
pk <pk@pksoftware.de>
2023-04-22 13:41:30 +0200

    button_levers

--- features/buttons-levers/js/buttons-levers.js
+++ features/buttons-levers/js/buttons-levers.js
@@ -23,35 +23,28 @@ fnConnect = function () {
         clientId: "metaphone-test"
     });
 
-    oFeature.messageMapping("button_toggle", (oEvent) => {
+    oFeature.featureStreamMapping("item_collect", (oEvent) => {
         const oData = oEvent.body;
-        if(oData.gameObject === "button_on_off_demo"){
-            document.getElementById("button_on_off_demo").classList.toggle("active", oData.value === "0.75");
-        }
-        else if(oData.gameObject === "switch_demo"){
-            document.getElementById("switch_demo").classList.toggle("active", oData.value === "0.75");
-        }
-    });
+        terminalLog("Received message from item: id=" + oData.itemId + ", name=" + oData.itemName);
 
-    oFeature.addEventListener('ready', (event) => {
-        //oFeature.sendMessageToEnvScript("button_controller", "setup");
-
-        //TODO implement message mapping for generic feature requests without fhClient ID
-        oFeature.subscribeToFeatureStream(false, (oData, mHeaders) => {
-            terminalLog("Received message from item: id=" + oData.itemId + ", name=" + oData.itemName);
-
+        const bSuccess = true;//Math.random() < 0.5;
+        if(bSuccess){
             const elCoinsCounter = document.getElementById("coins-counter");
             elCoinsCounter.textContent = parseInt(elCoinsCounter.textContent) + 1;
-            //Respond
-            oFeature.sendMessage("fhtp://item/item-controller-123/item-callback", { 
-                itemName: oData.itemName,
-                itemId: oData.itemId,
-                success: Math.random() < 0.5
-            });
-        });
-
+        }
         
+        //Respond
+        oFeature.sendMessage("fhtp://item/item-controller-123/item-callback", { 
+            itemName: oData.itemName,
+            itemId: oData.itemId,
+            success: bSuccess
+        });
     });
+
+    oFeature.featureStreamMapping("button", (oEvent) => {
+        document.getElementById("button-star").classList.toggle("active", oEvent.body.value === "ACTIVE");
+    });
+
     
     oFeature.connect();
 };
@@ -66,19 +59,7 @@ script.addEventListener('load', fnConnect);
 document.body.appendChild(script);
 
 
-document.getElementById("button_on_off_demo").addEventListener("click", function(){
-    /*
-    oFeature.sendMessageToEnvScript("button_controller", "set_state", {
-        action: "SET_STATE",
-        gameObject: "button_on_off_demo",
-        value: this.classList.contains("active") ? 0 : 0.75
-    });
-    */
-
-    oFeature.sendMessage("fhtp://item/item-controller-123/env/script/door_controller/open_close", { "value" : "1234" });
-});
-
-document.getElementById("switch_demo").addEventListener("click", function(){
+document.getElementById("button-star").addEventListener("click", function(){
     /*
     oFeature.sendMessageToEnvScript("button_controller", "set_state", {
         action: "SET_STATE",
